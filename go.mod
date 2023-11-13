13ececed4579f77e08d29fadb44c9c6315fc6047
joil <joil-z@outlook.com>
2023-11-13 16:58:30 +0800

    feat: 添加 jwt 相关插件

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/golang-jwt/jwt/v5 v5.1.0
 	github.com/redis/go-redis/v9 v9.3.0
 	github.com/stretchr/testify v1.8.4
 	go.uber.org/mock v0.3.0
---
+++ jwt/claims_option.go
@@ -0,0 +1,83 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Options struct {
+	Expire        time.Duration     // 有效期
+	EncryptionKey string            // 加密密钥
+	DecryptKey    string            // 解密密钥
+	Method        jwt.SigningMethod // 签名方式
+	Issuer        string            // 签发人
+	genIDFn       func() string     // 生成 JWT ID (jti) 的函数
+}
+
+// NewOptions 定义一个 JWT 配置.
+// DecryptKey: 默认与 EncryptionKey 相同.
+// Method: 默认使用 jwt.SigningMethodHS256 签名方式.
+func NewOptions(expire time.Duration, encryptionKey string,
+	opts ...Option) Options {
+	dOpts := Options{
+		Expire:        expire,
+		EncryptionKey: encryptionKey,
+		DecryptKey:    encryptionKey,
+		Method:        jwt.SigningMethodHS256,
+		genIDFn:       func() string { return "" },
+	}
+
+	for _, opt := range opts {
+		opt.apply(&dOpts)
+	}
+
+	return dOpts
+}
+
+type Option interface {
+	apply(*Options)
+}
+
+type funcOption struct {
+	f func(*Options)
+}
+
+func (fdo *funcOption) apply(do *Options) {
+	fdo.f(do)
+}
+
+func newFuncOption(f func(*Options)) *funcOption {
+	return &funcOption{
+		f: f,
+	}
+}
+
+// WithDecryptKey 设置解密密钥.
+func WithDecryptKey(decryptKey string) Option {
+	return newFuncOption(func(o *Options) {
+		o.DecryptKey = decryptKey
+	})
+}
+
+// WithMethod 设置 JWT 的签名方法.
+func WithMethod(method jwt.SigningMethod) Option {
+	return newFuncOption(func(o *Options) {
+		o.Method = method
+	})
+}
+
+// WithIssuer 设置签发人.
+func WithIssuer(issuer string) Option {
+	return newFuncOption(func(o *Options) {
+		o.Issuer = issuer
+	})
+}
+
+// WithGenIDFunc 设置生成 JWT ID 的函数.
+// 可以设置成 WithGenIDFunc(uuid.NewString).
+func WithGenIDFunc(fn func() string) Option {
+	return newFuncOption(func(o *Options) {
+		o.genIDFn = fn
+	})
+}
