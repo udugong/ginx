f457eb3ecefc6a1e1072ffc09bcb840ee4f2b891
joil <joil-z@outlook.com>
2023-12-29 15:47:36 +0800

    docs: 修改 jwt 相关使用示例

--- example/jwt/main.go
+++ example/jwt/main.go
@@ -21,7 +21,7 @@ func main() {
 
 	accessKey := "access key"
 	// 创建资源令牌管理服务
-	accessTM := jwtcore.NewTokenManagerServer[Claims, *Claims](10*time.Minute, accessKey)
+	accessTM := jwtcore.NewTokenManagerServer[Claims, *Claims](accessKey, 10*time.Minute)
 	// 创建认证中间件构建器
 	builder := ujwt.NewMiddlewareBuilder[Claims, *Claims](accessTM)
 
@@ -33,52 +33,52 @@ func main() {
 	// r.Use(gloAuthMiddleware)
 
 	// 单独拦截
-	r.GET("/profile", builder.Build(), func(ctx *gin.Context) {
-		ctx.Status(http.StatusOK)
+	r.GET("/profile", builder.Build(), func(c *gin.Context) {
+		c.Status(http.StatusOK)
 	})
 
 	// 登录设置资源令牌
-	r.POST("/login", func(ctx *gin.Context) {
+	r.POST("/login", func(c *gin.Context) {
 		// ...
 		// 如果校验成功
 		token, err := accessTM.GenerateToken(Claims{Uid: 1})
 		if err != nil {
-			ctx.Status(http.StatusInternalServerError)
+			c.Status(http.StatusInternalServerError)
 			return
 		}
-		ctx.Header("x-access-token", token)
-		ctx.Status(http.StatusNoContent)
+		c.Header("x-access-token", token)
+		c.Status(http.StatusNoContent)
 	})
 
 	// 使用刷新令牌处理函数
 	refreshKey := "refresh key"
 	// 创建刷新令牌的管理服务
-	refreshTM := jwtcore.NewTokenManagerServer[Claims, *Claims](24*time.Hour, refreshKey)
+	refreshTM := jwtcore.NewTokenManagerServer[Claims, *Claims](refreshKey, 24*time.Hour)
 	// 创建刷新令牌函数的构建器
-	refreshHdlBuilder := ujwt.NewRefreshHandlerBuilder[Claims, *Claims](accessTM, refreshTM)
+	refreshManager := ujwt.NewRefreshManager[Claims, *Claims](accessTM, refreshTM)
 
 	// 登录
-	r.POST("/login-v1", func(ctx *gin.Context) {
+	r.POST("/login-v1", func(c *gin.Context) {
 		// ...
 		// 如果校验成功
 		accessToken, err := accessTM.GenerateToken(Claims{Uid: 1})
 		if err != nil {
-			ctx.Status(http.StatusInternalServerError)
+			c.Status(http.StatusInternalServerError)
 			return
 		}
 		refreshToken, err := refreshTM.GenerateToken(Claims{Uid: 1})
 		if err != nil {
-			ctx.Status(http.StatusInternalServerError)
+			c.Status(http.StatusInternalServerError)
 			return
 		}
-		ctx.Header("x-access-token", accessToken)
-		ctx.Header("x-refresh-token", refreshToken)
-		ctx.Status(http.StatusNoContent)
+		c.Header("x-access-token", accessToken)
+		c.Header("x-refresh-token", refreshToken)
+		c.Status(http.StatusNoContent)
 	})
 
 	// 刷新令牌的函数
-	// 内部已经开启了 refresh 认证,因此可以直接与 relativePath 绑定
-	r.POST("/refresh-token", refreshHdlBuilder.Build)
+	// 内部已经开启了 refresh 令牌的认证,因此可以直接使用 refreshManager.Handler 与 relativePath 绑定
+	r.POST("/refresh-token", refreshManager.Handler)
 
 	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
 }
